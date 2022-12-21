1d9153246b985ab291144c2eec5e9ed1f976cf3f
lakshmi1129 <lakhsmipriya1129@gmail.com>
2022-12-21 10:42:57 +0530

    cart-server v4

--- services/dataservice.js
+++ services/dataservice.js
@@ -1,98 +1,114 @@
 const db = require('./db')
 
 // getAllProducts
-const getAllProducts= ()=>{
-   return  db.Product.find()
-   .then((data)=>{
-    if(data){
-        return{
-            statusCode: 200,
-            result:data
-        }
-    }
-    else{
-        return{
-        statusCode: 404,
-        message : 'Failed to fetch data'
-        }
-    }
-   })
+const getAllProducts = () => {
+    return db.Product.find()
+        .then((data) => {
+            if (data) {
+                return {
+                    statusCode: 200,
+                    result: data
+                }
+            }
+            else {
+                return {
+                    statusCode: 404,
+                    message: 'Failed to fetch data'
+                }
+            }
+        })
 }
 
 
 // addToWishlist
-const addToWishlist=(id,title,price,description,image)=>{
+const addToWishlist = (id, title, price, description, image) => {
     return db.Wishlist.findOne({
         id
-    }).then ((result)=> {
-        if(result){
-            return{
-                statusCode:404,
-                message:'Product already in your wishlist'
+    }).then((result) => {
+        if (result) {
+            return {
+                statusCode: 404,
+                message: 'Product already in your wishlist'
             };
         }
-        else{
+        else {
             const newProduct = new db.Wishlist({
-                 id,
-                 title,
-                 price,
-                 description,
-                 image
+                id,
+                title,
+                price,
+                description,
+                image
             });
             newProduct.save()
-            return{
-                statusCode:200,
-                message:"Product Successfully added to WishList"
+            return {
+                statusCode: 200,
+                message: "Product Successfully added to WishList"
             }
         }
-     });
+    });
 };
 
 // getWishlist
-const getWishlist = ()=>{
-    return  db.Wishlist.find()
-    .then((data)=>{
-     if(data){
-         return{
-             statusCode: 200,
-             result:data
-         }
-     }
-     else{
-         return{
-         statusCode: 404,
-         message : 'Your Wishlist is Empty'
-         }
-     }
-    })
+const getWishlist = () => {
+    return db.Wishlist.find()
+        .then((data) => {
+            if (data) {
+                return {
+                    statusCode: 200,
+                    result: data
+                }
+            }
+            else {
+                return {
+                    statusCode: 404,
+                    message: 'Your Wishlist is Empty'
+                }
+            }
+        })
 }
 
 
 // deleteFromWish
-const deleteFromWish = (id)=>{
+const deleteFromWish = (id) => {
     return db.Wishlist.deleteOne({
         id
     })
-    .then(
-        (data)=>{
-            if(data){
-                return{
-                    statusCode:200,
-                    message:'Product Removed from your wishlist'
+        .then(
+            (data) => {
+                if (data) {
+                    // return{
+                    //     statusCode:200,
+                    //     message:'Product Removed from your wishlist'
+                    // }
+                    return db.Wishlist.find()
+                        .then((data) => {
+                            if (data) {
+                                return {
+                                    statusCode: 200,
+                                    wishlist: data,
+                                    message:'Product Removed from your wishlist'
+                                }
+                            }
+                            else {
+                                return {
+                                    statusCode: 404,
+                                    message: 'Your Wishlist is Empty'
+                                }
+                            }
+                        })
+                }
+                else {
+                    return {
+                        statusCode: 404,
+                        message: 'Product not available'
+                    };
                 }
             }
-            else{
-                return{
-                    statusCode:404,
-                    message:'Product not available'
-                };
-            }
-        }
-    )
+        )
 }
 
 
-module.exports={
+module.exports = {
     getAllProducts,
     addToWishlist,
     getWishlist,
